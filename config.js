1a0390d9bd86ab3fd46257fe705100f623f0831d
vanclp <vanclp@vanclps-MacBook-Pro.local>
2018-01-13 00:29:36 +0800

    initial commit

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.port = 5001
+exports.api = 'JuYkJu0d2gaGJ1fbjvhrLVUlvRz869ixRLMS0CsrzrxcVQmICrtnYcbtAajPWHEc5BHLi3dWfXy5nXwFOvTN_-7DHtWihLI6_DNlb9Qbcz8KplJlYYTahCPQwHVYWnYx'
+exports.url = 'https://api.yelp.com/v3/businesses'
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const config = require('./config')
+const fetch = require('node-fetch')
+
+const app = express()
+
+app.use(cors())
+
+app.get('/location/:id', (req, res) => {
+  const url = `${config.url}/${req.params.id}`
+  fetch(url, { headers: { Authorization: `Bearer ${config.api}` } })
+    .then(response => response.text())
+    .then(body => res.send(body))
+    .catch(err => {
+      res.status(500)
+    })
+})
+
+app.listen(config.port, () => {
+  console.log('Server listening on port %s, Ctrl+C to stop', config.port)
+})
